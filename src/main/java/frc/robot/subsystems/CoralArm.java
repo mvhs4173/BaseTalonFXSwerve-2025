be01be2bf47d4aa9d51cf200d24e6225c013dcc1
BillDunlap <williamwdunlap@gmail.com>
2025-03-19 11:05:29 -0700

    Fix bang-bang control of wrist and general cleanup

--- src/main/java/frc/robot/subsystems/CoralArm.java
+++ src/main/java/frc/robot/subsystems/CoralArm.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+//import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.CANId;
 import frc.robot.Constants;
 
@@ -20,22 +20,26 @@ public class CoralArm extends SubsystemBase {
   private final SparkMaxMotor m_wristMotor;
   private final SparkMaxMotor m_rollerMotor;
   private final OnOffSwitch m_CoralDetectionSensor;
-  private final double m_wristMotorRotationLimit = 0.25;
-  private final double m_ROLLERINWARDPERCENTSPEED = 0.35; 
-  private final double m_ROLLEROUTWARDPERCENTSPEED = -0.35; 
-  private final double m_wristPercentSpeed = 0.1; //TODO: adjust these
-  private double m_wristDesiredPercentSpeed = 0.0;
+
+  private final double m_ROLLER_INWARD_PERCENT_SPEED =  +0.35; 
+  private final double m_ROLLER_OUTWARD_PERCENT_SPEED = -0.35; 
+
   private boolean m_isExtended = false;
-  private double m_wristPosition;
-  private boolean m_isCoralInIntake;
+  private boolean m_isCoralInIntake = false;
   private boolean m_isWristVertical;
   private boolean m_isWristHorizontal;
-  private double m_rollerCurrentDraw;
-  private double m_tolerance = (2.0 / 360.0); // May need tuning
-  private CoralIntakeInfo m_coralIntakeInfo;
-  private double m_wristDesiredPosition = 0.0;
+  // private double m_rollerCurrentDraw;
 
-  
+  private final double m_WRIST_LOWER_POSITION_LIMIT =  0.00;
+  private final double m_WRIST_UPPER_POSITION_LIMIT = +0.25;
+  private final double m_WRIST_POSITION_TOLERANCE = (2.0 / 360.0); // May need tuning 
+  private double m_wristDesiredPositionRotations = 0.0;
+  private double m_wristPositionRotations;
+
+  private final double m_WRIST_ABS_MAX_PERCENT_SPEED = 0.10; //TODO: adjust these
+  private double m_wristDesiredPercentSpeed = 0.0;
+
+  private CoralIntakeInfo m_coralIntakeInfo;
 
   /** Creates a new CoralArm. */
   public CoralArm(CANId wristCAN, CANId rollerCAN, int OnOffSwitchChannel) {
@@ -66,35 +70,25 @@ public class CoralArm extends SubsystemBase {
   }
 
   private void setWristPercentSpeed(double percent){
-    if (Math.abs(m_wristPosition) < m_wristMotorRotationLimit){
+    if (Math.abs(m_wristPositionRotations) < m_WRIST_LOWER_POSITION_LIMIT){
       m_wristMotor.setPercentSpeed(percent);
     }
   }
 
-  public Command wristGoToPosition(double r_desiredPositionRotations){
-    
+  public Command wristGoToPosition(double desiredPositionRotations){
+    m_wristDesiredPositionRotations = MathUtil.clamp(desiredPositionRotations, m_WRIST_LOWER_POSITION_LIMIT, m_WRIST_UPPER_POSITION_LIMIT);
     return run(() -> {
-      m_wristDesiredPosition = r_desiredPositionRotations;
-      double desiredPositionRotations = MathUtil.clamp(r_desiredPositionRotations, 0, 90.0 / 360.0);
-      if(Math.abs(desiredPositionRotations) > m_wristMotorRotationLimit){
-        if(Math.abs(m_wristMotorRotationLimit - m_wristPosition) < m_tolerance){
-          m_wristDesiredPercentSpeed = 0.0;
-        } else if(m_wristMotorRotationLimit > m_wristPosition){
-          m_wristDesiredPercentSpeed = m_wristPercentSpeed;
-        } else if(m_wristMotorRotationLimit < m_wristPosition){
-          m_wristDesiredPercentSpeed = -m_wristPercentSpeed;
-        }
-       } else {
-        if(Math.abs(desiredPositionRotations - m_wristPosition) < m_tolerance){
-          m_wristDesiredPercentSpeed = 0.0;
-        } else if(desiredPositionRotations > m_wristPosition){
-          m_wristDesiredPercentSpeed = m_wristPercentSpeed;
-        } else if(desiredPositionRotations < m_wristPosition){
-          m_wristDesiredPercentSpeed = -m_wristPercentSpeed;
-        }
+      double error = m_wristDesiredPositionRotations - m_wristPositionRotations; // current position is updated in periodic()
+      // bang-bang control:
+      if(Math.abs(error) <= m_WRIST_POSITION_TOLERANCE){
+        m_wristDesiredPercentSpeed = 0.0;
+      } else if(error > 0.0){
+        m_wristDesiredPercentSpeed = m_WRIST_ABS_MAX_PERCENT_SPEED;
+      } else {
+        m_wristDesiredPercentSpeed = -m_WRIST_ABS_MAX_PERCENT_SPEED;
       }
+      // expect speed to be set in periodic()
     });
-
   }
 
   private double getCoralRollerCurrent(){
@@ -121,16 +115,16 @@ public class CoralArm extends SubsystemBase {
     () -> {
       m_coralIntakeInfo.hasHitTopSpeed = m_coralIntakeInfo.hasHitTopSpeed || (m_rollerMotor.getVelocity() < CoralIntakeInfo.TOP_SPEED);
       m_coralIntakeInfo.hasHitHighCurrent =
-      m_coralIntakeInfo.hasHitHighCurrent
+        m_coralIntakeInfo.hasHitHighCurrent
         || (m_coralIntakeInfo.hasHitTopSpeed && m_coralIntakeInfo.debouncer.calculate((getCoralRollerCurrent() > CoralIntakeInfo.HIGH_CURRENT)));
-      setRollerPercentSpeed(m_coralIntakeInfo.hasHitHighCurrent ? 0.0 : m_ROLLERINWARDPERCENTSPEED);
+      setRollerPercentSpeed(m_coralIntakeInfo.hasHitHighCurrent ? 0.0 : m_ROLLER_INWARD_PERCENT_SPEED);
     }).withName("Algae Roller Intake");
 
   }
 
   public Command rollerPushOut(){
     return run(() -> {
-      setRollerPercentSpeed(m_ROLLEROUTWARDPERCENTSPEED);
+      setRollerPercentSpeed(m_ROLLER_OUTWARD_PERCENT_SPEED);
     });
   }
 
@@ -151,27 +145,19 @@ public class CoralArm extends SubsystemBase {
   }
 
   public boolean isWristVertical(){
-    if (Math.abs(0.25 - m_wristPosition) < m_tolerance) {
-      return true;
-    } else {
-      return false;
-    }
+    return Math.abs(0.25 - m_wristPositionRotations) < m_WRIST_POSITION_TOLERANCE;
   }
 
   public boolean isWristHorizontal(){
-    if (Math.abs(0.0 - m_wristPosition) < m_tolerance) {
-      return true;
-    } else {
-      return false;
-    }
+    return Math.abs(0.0 - m_wristPositionRotations) < m_WRIST_POSITION_TOLERANCE;
   }
   /*Triggers: coral collected, arm extended, wrist vertical,
    wrist horizontal, arm retracted*/
-  public Trigger isCoralInIntake = new Trigger(() -> m_isCoralInIntake);
-  public Trigger isArmExtended = new Trigger(() -> m_isExtended);
-  public Trigger isWristVertical = new Trigger(() -> m_isWristVertical);
-  public Trigger isWristHorizontal = new Trigger(() -> m_isWristHorizontal);
-  public Trigger isArmRetracted = new Trigger(() -> !m_isExtended);
+  //public Trigger isCoralInIntakeTrigger = new Trigger(() -> m_isCoralInIntake);
+  //public Trigger isArmExtendedTrigger = new Trigger(() -> m_isExtended);
+  //public Trigger isWristVerticalTrigger = new Trigger(() -> m_isWristVertical);
+  //public Trigger isWristHorizontalTrigger = new Trigger(() -> m_isWristHorizontal);
+  //public Trigger isArmRetractedTrigger = new Trigger(() -> !m_isExtended);
 
 
 
@@ -179,7 +165,7 @@ public class CoralArm extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    m_wristPosition = m_wristMotor.getPosition();
+    m_wristPositionRotations = m_wristMotor.getPosition();
     m_isCoralInIntake = isCoralInIntake();
     m_isWristVertical = isWristVertical();
     m_isWristHorizontal = isWristHorizontal();
@@ -188,8 +174,8 @@ public class CoralArm extends SubsystemBase {
     SmartDashboard.putBoolean("Is wrist Vertical", m_isWristVertical);
     SmartDashboard.putBoolean("Is wrist Horizontal", m_isWristHorizontal);
     SmartDashboard.putBoolean("Is Coral in intake", m_isCoralInIntake);
-    SmartDashboard.putNumber("Wrist Position", m_wristPosition);
+    SmartDashboard.putNumber("Wrist Position", m_wristPositionRotations);
     SmartDashboard.putBoolean("Is coral arm extended", m_isExtended);
-    SmartDashboard.putNumber("Wrist desired position", m_wristDesiredPosition);
+    SmartDashboard.putNumber("Wrist desired position", m_wristDesiredPositionRotations);
   }
 }
